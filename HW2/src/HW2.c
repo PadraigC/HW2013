68abf2c162c445a17cbbd501146f1ab0bdb2680a
Padraig Cunningham <padraig.cunningham@ucd.ie>
2013-01-15 20:13:08 +0000

    asas

--- HW2/src/HW2.c
+++ HW2/src/HW2.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!! 2013 XoX"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!! 2013 XoxxxX"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
